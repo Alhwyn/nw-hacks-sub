2043277647eff0073801f652c798bffb01951535
Caly Zheng <calyzheng1@gmail.com>
2026-01-17 19:42:39 -0800

    rough draft of UI

--- index.ts
+++ index.ts
@@ -1,1 +1,42 @@
-console.log("Hello via Bun!");
+import { app, BrowserWindow, ipcMain } from 'electron/main'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        transparent: true,
+        frame: false,
+        alwaysOnTop: true,
+        resizable: false,
+        webPreferences: {
+            preload: path.join(path.dirname(fileURLToPath(import.meta.url)), 'preload.js'),
+    contextIsolation: true,
+    nodeIntegration: false,
+    }
+  })
+
+  win.loadFile('src/renderer/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+
+ipcMain.on('close', () => {
+  app.quit()
+})
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+export default {
+  content: ['./renderer/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
